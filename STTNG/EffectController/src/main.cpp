1f3721125b1f710a9cdb11f3bbe1d19f06347d86
Markus Kalkbrenner <git@kalki.de>
2021-03-01 18:59:55 +0100

    added WS1812FX based effect

--- STTNG/EffectController/src/main.cpp
+++ STTNG/EffectController/src/main.cpp
@@ -1,47 +1,12 @@
 // Markus Kalkbrenner 2020
 // Note to self: Play more pinball!
 
+#include <PPUCDefinitions.h>
+
+#define PPUC_NUM_LEDS_1 1
+#define PPUC_LED_TYPE_1 SK6812_GRBW
+
 #include <PPUCEffectsController.h>
-#include <PPUCEvent.h>
-#include <PPUCEffects/PPUCLedBlinkEffect.h>
-#include <PPUCEffects/PPUCNullEffect.h>
-#include <PPUCEffects/PPUCRGBColorCycleEffect.h>
-#include <PPUCCrossLinkDebugger.h>
-
-const int   ws2812NumLeds1 = 1;                        // number of LEDs
-const int   ws2812Type1    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory1[ws2812NumLeds1 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory1[ws2812NumLeds1 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds2 = 0;                        // number of LEDs
-const int   ws2812Type2    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory2[ws2812NumLeds2 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory2[ws2812NumLeds2 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds3 = 0;                        // number of LEDs
-const int   ws2812Type3    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory3[ws2812NumLeds3 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory3[ws2812NumLeds3 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds4 = 0;                        // number of LEDs
-const int   ws2812Type4    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory4[ws2812NumLeds4 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory4[ws2812NumLeds4 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds5 = 0;                        // number of LEDs
-const int   ws2812Type5    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory5[ws2812NumLeds5 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory5[ws2812NumLeds5 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds6 = 0;                        // number of LEDs
-const int   ws2812Type6    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory6[ws2812NumLeds6 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory6[ws2812NumLeds6 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
-
-const int   ws2812NumLeds7 = 0;                        // number of LEDs
-const int   ws2812Type7    = WS2812_GRBW;              // type of LEDs
-byte        ws2812DrawingMemory7[ws2812NumLeds7 * 4];  // 3 bytes per LED for RGB, 4 bytes for RGBW
-DMAMEM byte ws2812DisplayMemory7[ws2812NumLeds7 * 16]; // 12 bytes per LED for RGB, 16 bytes for RGBW
 
 PPUCEffectsController effectsController("Teensy4.1");
 
@@ -62,7 +27,6 @@ void setup() {
          0  // mode
     );
 
-
     effectsController.addEffect(
          new PPUCNullEffect(),
          effectsController.ledBuiltInDevice(),
@@ -73,13 +37,25 @@ void setup() {
     );
 
     effectsController.addEffect(
-         new PPUCRGBColorCycleEffect(1),
-         effectsController.ws2812Serial(1),
+         new PPUCWS2812FXRainbowCycle(),
+         effectsController.ws2812SerialDevice(1),
+//         effectsController.ws2812SerialOverlayDevice(1, 0, 0, 1),
          new PPUCEvent(EVENT_SOURCE_EFFECT, 1, 255), // controller start event
          1, // priority
          -1, // run endless
          0  // mode
     );
+
+/*
+    effectsController.addEffect(
+            new PPUCRGBColorCycleEffect(1),
+            effectsController.ws2812SerialDevice(1),
+            new PPUCEvent(EVENT_SOURCE_EFFECT, 1, 255), // controller start event
+            1, // priority
+            -1, // run endless
+            0  // mode
+    );
+*/
 
     // Start
     effectsController.start();
